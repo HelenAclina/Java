8bf2cffd3794bfbf4bb0e49e4326315d83c9187d
Helen Aclina <168231743+HelenAclina@users.noreply.github.com>
2026-03-11 16:07:37 -0300

    Add files via upload

---
+++ CusoDeJava/Exercicios/HoraDoSistema/src/main/java/horadosistema/horadosistema/HoraDoSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package horadosistema.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aclin
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date ();
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+    }
+}
