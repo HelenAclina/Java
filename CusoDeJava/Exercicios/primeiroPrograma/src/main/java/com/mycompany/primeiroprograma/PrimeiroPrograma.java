de49db71020f957951f03e4698f00fc14ecba850
Helen Aclina <168231743+HelenAclina@users.noreply.github.com>
2026-03-11 16:05:59 -0300

    Add exercise from lesson 04 - Curso Em Video

---
+++ CusoDeJava/Exercicios/primeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author aclin
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
